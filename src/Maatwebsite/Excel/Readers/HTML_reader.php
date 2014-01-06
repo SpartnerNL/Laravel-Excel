84793031896ae7bf73bc4a83830604c7ce6f5aff
Patrick Brouwers <info@patrickbrouwersfilm.com>
2014-01-06 19:52:23 +0100

    + Autosize HTML columns, + Styling for HTML to XLS

--- src/Maatwebsite/Excel/Readers/HTML_reader.php
+++ src/Maatwebsite/Excel/Readers/HTML_reader.php
@@ -30,6 +30,8 @@ namespace Maatwebsite\Excel\Readers;
 
 use \PHPExcel;
 use \PHPExcel_Reader_HTML;
+use \PHPExcel_Style_Color;
+use \PHPExcel_Style_Border;
 
 class HTML_reader extends \PHPExcel_Reader_HTML
 {
@@ -47,6 +49,77 @@ class HTML_reader extends \PHPExcel_Reader_HTML
      * @var int
      */
     private $_sheetIndex    = 0;
+
+    private $_formats = array(
+        'h1' => array(
+            'font' => array(
+                'bold' => true,
+                'size' => 24,
+            ),
+        ),
+        'strong' => array(
+            'font' => array(
+                'bold' => true,
+                'size' => 12,
+            ),
+        ),
+        'b' => array(
+            'font' => array(
+                'bold' => true,
+                'size' => 12,
+            ),
+        ),
+        'i' => array(
+            'font' => array(
+                'italic' => true,
+                'size' => 12,
+            ),
+        ),  //  Bold, 24pt
+       'h2' => array(
+            'font' => array(
+                'bold' => true,
+                'size' => 18,
+                ),
+            ),  //  Bold, 18pt
+       'h3' => array(
+            'font' => array(
+                'bold' => true,
+                'size' => 13.5,
+                ),
+            ),  //  Bold, 13.5pt
+       'h4' => array(
+            'font' => array(
+                'bold' => true,
+                'size' => 12,
+                ),
+            ),  //  Bold, 12pt
+       'h5' => array(
+            'font' => array(
+                'bold' => true,
+                'size' => 10,
+                ),
+            ),  //  Bold, 10pt
+       'h6' => array(
+            'font' => array(
+                'bold' => true,
+                'size' => 7.5,
+                ),
+            ),  //  Bold, 7.5pt
+       'a'  => array(
+            'font' => array(
+                'underline' => true,
+                'color' => array( 'argb' => PHPExcel_Style_Color::COLOR_BLUE),
+            ),
+        ),  //  Blue underlined
+       'hr' => array(
+            'borders' => array(
+                'bottom' => array(
+                    'style' => PHPExcel_Style_Border::BORDER_THIN,
+                    'color' => array(PHPExcel_Style_Color::COLOR_BLACK)
+                ),
+            ),
+        ),  //  Bottom border
+     );
 
     /**
      * Loads PHPExcel from file
@@ -115,15 +188,35 @@ class HTML_reader extends \PHPExcel_Reader_HTML
         }
 
         //  Discard white space
-        $dom->preserveWhiteSpace = false;
+        $dom->preserveWhiteSpace = true;
 
 
         $row = 0;
         $column = 'A';
         $content = '';
         $this->_processDomElement($dom,$objPHPExcel->getActiveSheet(),$row,$column,$content);
+        $this->autosizeColumn($objPHPExcel);
 
         return $objPHPExcel;
+    }
+
+    /**
+     * Autosize column for document
+     *
+     * @return int
+     */
+    public static function autosizeColumn(\PHPExcel $objPHPExcel)
+    {
+        foreach ($objPHPExcel->getAllSheets() as $sheet) {
+            $toCol = $sheet->getHighestColumn();
+
+            $toCol++;
+            for ($i = 'A'; $i !== $toCol; $i++) {
+                $sheet->getColumnDimension($i)->setAutoSize(true);
+            }
+
+            $sheet->calculateColumnWidths();
+        }
     }
 
     private function _processDomElement(\DOMNode $element, $sheet, &$row, &$column, &$cellContent){
@@ -170,12 +263,19 @@ class HTML_reader extends \PHPExcel_Reader_HTML
                     case 'em'    :
                     case 'strong':
                     case 'b'     :
+
 //                      echo 'STYLING, SPAN OR DIV<br />';
                         if ($cellContent > '')
                             $cellContent .= ' ';
                         $this->_processDomElement($child,$sheet,$row,$column,$cellContent);
                         if ($cellContent > '')
                             $cellContent .= ' ';
+
+                        // Set the styling
+                        if (isset($this->_formats[$child->nodeName])) {
+                            $sheet->getStyle($column.$row)->applyFromArray($this->_formats[$child->nodeName]);
+                        }
+
 //                      echo 'END OF STYLING, SPAN OR DIV<br />';
                         break;
                     case 'hr' :
@@ -206,6 +306,7 @@ class HTML_reader extends \PHPExcel_Reader_HTML
                                 case 'href':
 //                                  echo 'Link to ' , $attributeValue , '<br />';
                                     $sheet->getCell($column.$row)->getHyperlink()->setUrl($attributeValue);
+
                                     if (isset($this->_formats[$child->nodeName])) {
                                         $sheet->getStyle($column.$row)->applyFromArray($this->_formats[$child->nodeName]);
                                     }
@@ -225,11 +326,19 @@ class HTML_reader extends \PHPExcel_Reader_HTML
                     case 'ol' :
                     case 'ul' :
                     case 'p'  :
+
                         if ($this->_tableLevel > 0) {
                             //  If we're inside a table, replace with a \n
                             $cellContent .= "\n";
 //                          echo 'LIST ENTRY: ' , '<br />';
                             $this->_processDomElement($child,$sheet,$row,$column,$cellContent);
+
+                            $this->_flushCell($sheet,$column,$row,$cellContent);
+
+                            if (isset($this->_formats[$child->nodeName])) {
+                                $sheet->getStyle($column.$row)->applyFromArray($this->_formats[$child->nodeName]);
+                            }
+
 //                          echo 'END OF LIST ENTRY:' , '<br />';
                         } else {
                             if ($cellContent > '') {
