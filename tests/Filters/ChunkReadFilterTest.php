52fe9e7cd4674126e25742c19975f76024ab3d9b
Muhammad Faisal Bin Arbain <faisal.arbain@tm.com.my>
2014-09-30 04:52:31 +0800

    Make first chunk to follow chunk size requested

--- tests/Filters/ChunkReadFilterTest.php
+++ tests/Filters/ChunkReadFilterTest.php
@@ -15,44 +15,44 @@ class ChunkReadFilterTest extends TestCase {
     public function testCanChunkXls()
     {
     	$this->assertCanChunkIntoGroups(1,"sample.xls",20);
-    	$this->assertCanChunkIntoGroups(2,"sample.xls",15);
+    	$this->assertCanChunkIntoGroups(1,"sample.xls",15);
     	$this->assertCanChunkIntoGroups(2,"sample.xls",10);
-    	$this->assertCanChunkIntoGroups(4,"sample.xls",5);
+    	$this->assertCanChunkIntoGroups(3,"sample.xls",5);
     }    
 
     public function testCanChunkXlsx()
     {
     	$this->assertCanChunkIntoGroups(1,"sample.xlsx",20);
-    	$this->assertCanChunkIntoGroups(2,"sample.xlsx",15);
+    	$this->assertCanChunkIntoGroups(1,"sample.xlsx",15);
     	$this->assertCanChunkIntoGroups(2,"sample.xlsx",10);
-    	$this->assertCanChunkIntoGroups(4,"sample.xlsx",5);
+    	$this->assertCanChunkIntoGroups(3,"sample.xlsx",5);
     }    
 
     public function testCanChunkCsv()
     {
     	$this->assertCanChunkIntoGroups(1,"sample.csv", 20);
-    	$this->assertCanChunkIntoGroups(2,"sample.csv", 15);
+    	$this->assertCanChunkIntoGroups(1,"sample.csv", 15);
     	$this->assertCanChunkIntoGroups(2,"sample.csv", 10);
-    	$this->assertCanChunkIntoGroups(4,"sample.csv", 5);
+    	$this->assertCanChunkIntoGroups(3,"sample.csv", 5);
     }
 
-    private function assertCanChunkIntoGroups($expected_rounds, $file, $size)
+    private function assertCanChunkIntoGroups($expected_chunks, $file, $chunk_size)
     {
     	$output = [];
         
         $rounds = 0;
 
-        $this->excel->filter('chunk')->load(__DIR__ . "/files/{$file}")->chunk($size,function($results) use (&$output, &$rounds){
+        $this->excel->filter('chunk')->load(__DIR__ . "/files/{$file}")->chunk($chunk_size,function($results) use (&$output, &$rounds){
         	$rounds++;
-        	foreach ($results as $row) {
+            foreach ($results as $row) {
         		$output[] = (int) $row->header;
         	}
         });
 
         $expected = "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15";
 
-        $this->assertEquals($expected, implode(",", $output ));
-        $this->assertEquals($expected_rounds, $rounds);
+        $this->assertEquals($expected, implode(",", $output ), "Chunked ($chunk_size) value not equal with source data.");
+        $this->assertEquals($expected_chunks, $rounds, "Expecting total chunks is $expected_rounds when chunk with size $chunk_size");
 
     }
 }
