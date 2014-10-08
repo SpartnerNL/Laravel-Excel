2871be27b1610d8d421488e2efedae6245ea9271
Patrick Brouwers <patrickbrouwersfilm@gmail.com>
2014-10-08 19:47:40 +0200

    Implement __isset() for CellCollection, fixes issue #246

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,30 +5,19 @@ use Orchestra\Testbench\TestCase as TestBenchTestCase;
 class TestCase extends TestBenchTestCase
 {
 
-    /**
-     * Test the binding with the ioC
-     * @return [type] [description]
-     */
     public function testExcelClass()
     {
         $excel = App::make('Maatwebsite\Excel\Excel');
         $this->assertInstanceOf('Maatwebsite\Excel\Excel', $excel);
     }
 
-    /**
-     * Get the package service provider
-     * @return [type] [description]
-     */
+
     protected function getPackageProviders()
     {
         return array('Maatwebsite\Excel\ExcelServiceProvider');
     }
 
-    /**
-     * Get the path for this package
-     *
-     * @return string
-     */
+
     protected function getPackagePath()
     {
         return realpath(implode(DIRECTORY_SEPARATOR, array(
