fd20bc9ec91f20825be708fe90337fab70ed3f6c
Devin Dombrowski - Locutus <devin@devindombrowski.com>
2015-02-12 17:54:30 -0800

    New Feature: ability to set and reset the PHPExcel Cell ValueBinder
    This new abilty allows a user to overide PHPExcel's default behavior/logic when reading a cell and trying to intelligently format it's value.

--- src/Maatwebsite/Excel/Readers/LaravelExcelReader.php
+++ src/Maatwebsite/Excel/Readers/LaravelExcelReader.php
@@ -3,7 +3,10 @@
 use Cache;
 use Config;
 use Maatwebsite\Excel\Classes\PHPExcel;
+use PHPExcel_Cell;
 use PHPExcel_IOFactory;
+use PHPExcel_Cell_IValueBinder;
+use PHPExcel_Cell_DefaultValueBinder;
 use Illuminate\Filesystem\Filesystem;
 use Maatwebsite\Excel\Parsers\ExcelParser;
 use Maatwebsite\Excel\Classes\FormatIdentifier;
@@ -681,6 +684,25 @@ class LaravelExcelReader {
         return $this;
     }
 
+    /**
+     * Set custom value binder
+     * @param string|boolean $ext
+     * @return void
+     */
+    public function setValueBinder(PHPExcel_Cell_IValueBinder $binder)
+    {
+        PHPExcel_Cell::setValueBinder($binder);
+    }
+
+    /**
+     * Reset the value binder back to default
+     * @return void
+     */
+    public function resetValueBinder()
+    {
+        PHPExcel_Cell::setValueBinder(new PHPExcel_Cell_DefaultValueBinder);
+    }
+    
     /**
      * Set the date format
      * @param bool|string $format The date format
