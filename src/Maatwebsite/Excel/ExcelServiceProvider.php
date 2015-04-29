642089f0d4ff0759b5e5784508ab0e7f798c2b1f
hexusdev <hexus.dev@gmail.com>
2015-04-29 18:35:21 +0300

    Update ExcelServiceProvider.php

--- src/Maatwebsite/Excel/ExcelServiceProvider.php
+++ src/Maatwebsite/Excel/ExcelServiceProvider.php
@@ -46,9 +46,6 @@ class ExcelServiceProvider extends ServiceProvider {
 
         // Set the autosizing settings
         $this->setAutoSizingSettings();
-
-        // Register filters
-        $this->registerFilters();
     }
 
     /**
@@ -171,12 +168,16 @@ class ExcelServiceProvider extends ServiceProvider {
         // Bind the Excel class and inject its dependencies
         $this->app['excel'] = $this->app->share(function ($app)
         {
-            return new Excel(
+            $excel = new Excel(
                 $app['phpexcel'],
                 $app['excel.reader'],
                 $app['excel.writer'],
                 $app['excel.parsers.view']
             );
+            $filters = Config::get('excel::filters', array());
+
+            $excel->registerFilters($filters);
+            return $excel;
         });
     }
 
