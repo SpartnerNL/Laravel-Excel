1648e567cb32fb522e7dbcddfba15e13fcca5116
Patrick Brouwers <patrickbrouwersfilm@gmail.com>
2017-06-25 01:32:24 +0200

    Add integration test for phpspreadsheet

--- tests/Drivers/PhpSpreadsheet/ReaderTest.php
+++ tests/Drivers/PhpSpreadsheet/ReaderTest.php
@@ -2,6 +2,7 @@
 
 namespace Maatwebsite\Excel\Tests\Drivers\PhpSpreadsheet;
 
+use Maatwebsite\Excel\Sheet;
 use PHPUnit\Framework\TestCase;
 use Maatwebsite\Excel\Configuration;
 use Maatwebsite\Excel\Drivers\PhpSpreadsheet\Reader;
@@ -78,5 +79,39 @@ class ReaderTest extends TestCase
 
         $this->assertInstanceOf(Spreadsheet::class, $spreadsheet);
         $this->assertEquals('Custom-loaded spreadsheet', $spreadsheet->getTitle());
+    }
+
+    /**
+     * @test
+     */
+    public function reader_can_load_and_parse_simple_xlsx()
+    {
+        $this->reader->load($this->simpleXlsx, function (Spreadsheet $spreadsheet) {
+
+            $spreadsheet->sheet('Simple', function (Sheet $sheet) {
+                $this->assertEquals('Simple', $sheet->getTitle());
+
+                $this->assertEquals(
+                    [
+                        ['A1', 'B1', 'C1', 'D1'],
+                        ['A2', 'B2', 'C2', 'D2'],
+                        ['A3', 'B3', 'C3', 'D3'],
+                        ['A4', 'B4', 'C4', 'D4'],
+                        ['A5', 'B5', 'C5', 'D5'],
+                        ['A6', 'B6', 'C6', 'D6'],
+                        ['A7', 'B7', 'C7', 'D7'],
+                        ['A8', 'B8', 'C8', 'D8'],
+                        ['A9', 'B9', 'C9', 'D9'],
+                        ['A10', 'B10', 'C10', 'D10'],
+                        ['A11', 'B11', 'C11', 'D11'],
+                    ],
+                    $sheet->toArray()
+                );
+
+                $this->assertEquals('B8', $sheet->cell('B8')->getValue());
+                $this->assertEquals(8, $sheet->row(8)->getRowNumber());
+                $this->assertEquals('C', $sheet->column('C')->getColumnIndex());
+            });
+        });
     }
 }
