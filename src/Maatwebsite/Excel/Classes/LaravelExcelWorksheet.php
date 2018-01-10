bd71c9f462ea4a7945944691cc58acd2c85abd47
Rahul Jain <rahul_jain@live.in>
2018-01-10 17:28:11 +0530

    fix for bug where count is used without variable being countable: #1465 (#1468)

--- src/Maatwebsite/Excel/Classes/LaravelExcelWorksheet.php
+++ src/Maatwebsite/Excel/Classes/LaravelExcelWorksheet.php
@@ -594,7 +594,7 @@ class LaravelExcelWorksheet extends PHPExcel_Worksheet {
             $firstRow = reset($array);
 
             // Check if the array has array values
-            if (count($firstRow) != count($firstRow, 1))
+            if (is_array($firstRow) && count($firstRow) != count($firstRow, 1))
             {
                 // Loop through the data to remove arrays
                 $data = [];
