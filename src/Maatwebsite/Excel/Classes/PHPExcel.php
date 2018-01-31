876d63c3e53322c1949fa75fcc165ce87576b56c
Gabriel Hans Gonzalez <ghans@intecfra.com>
2018-01-31 13:32:34 -0600

    Fixed ExcelServiceProvider and PHPExcel migrated from PHPExcel to SpreadSheet

--- src/Maatwebsite/Excel/Classes/PHPExcel.php
+++ src/Maatwebsite/Excel/Classes/PHPExcel.php
@@ -1,6 +1,6 @@
 <?php namespace Maatwebsite\Excel\Classes;
 
-use PHPExcel as PHPOffice_PHPExcel;
+use PhpOffice\PhpSpreadsheet\Spreadsheet as PHPOffice_PHPExcel;
 
 /**
  *
@@ -13,111 +13,111 @@ use PHPExcel as PHPOffice_PHPExcel;
  * @author     Maatwebsite <info@maatwebsite.nl>
  * @license    http://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt    LGPL
  */
-class PHPExcel extends PHPOffice_PHPExcel {
+class PHPExcel extends PHPOffice_PHPExcel
+{
 
-    /**
-     * Allowed autofill properties
-     * @var array
-     */
-    public $allowedProperties = [
-        'creator',
-        'lastModifiedBy',
-        'description',
-        'subject',
-        'keywords',
-        'category',
-        'manager',
-        'company'
-    ];
+	/**
+	 * Allowed autofill properties
+	 * @var array
+	 */
+	public $allowedProperties = [
+		'creator',
+		'lastModifiedBy',
+		'description',
+		'subject',
+		'keywords',
+		'category',
+		'manager',
+		'company'
+	];
 
-    /**
-     * Create sheet and add it to this workbook
-     *
-     * @param  int|null   $iSheetIndex Index where sheet should go (0,1,..., or null for last)
-     * @param bool|string $title
-     * @throws \PHPExcel_Exception
-     * @return LaravelExcelWorksheet
-     */
-    public function createSheet($iSheetIndex = null, $title = false)
-    {
-        // Init new Laravel Excel worksheet
-        $newSheet = new LaravelExcelWorksheet($this, $title);
+	/**
+	 * Create sheet and add it to this workbook
+	 *
+	 * @param  int|null $iSheetIndex Index where sheet should go (0,1,..., or null for last)
+	 * @param bool|string $title
+	 * @return LaravelExcelWorksheet
+	 * @throws \PhpOffice\PhpSpreadsheet\Exception
+	 */
+	public function createSheet($iSheetIndex = null, $title = false)
+	{
+		// Init new Laravel Excel worksheet
+		$newSheet = new LaravelExcelWorksheet($this, $title);
 
-        // Add the sheet
-        $this->addSheet($newSheet, $iSheetIndex);
+		// Add the sheet
+		$this->addSheet($newSheet, $iSheetIndex);
 
-        // Return the sheet
-        return $newSheet;
-    }
+		// Return the sheet
+		return $newSheet;
+	}
 
-    /**
-     * Check if the user change change the workbook property
-     * @param  string $method
-     * @return boolean
-     */
-    public function isChangeableProperty($method)
-    {
-        $name = lcfirst(str_replace('set', '', $method));
+	/**
+	 * Check if the user change change the workbook property
+	 * @param  string $method
+	 * @return boolean
+	 */
+	public function isChangeableProperty($method)
+	{
+		$name = lcfirst(str_replace('set', '', $method));
 
-        return in_array($name, $this->getAllowedProperties()) ? true : false;
-    }
+		return in_array($name, $this->getAllowedProperties()) ? true : false;
+	}
 
-    /**
-     * Set default properties
-     * @param array $custom
-     * @return  void
-     */
-    public function setDefaultProperties($custom = [])
-    {
-        // Get the properties
-        $properties = $this->getProperties();
+	/**
+	 * Set default properties
+	 * @param array $custom
+	 * @return  void
+	 */
+	public function setDefaultProperties($custom = [])
+	{
+		// Get the properties
+		$properties = $this->getProperties();
 
-        // Get fillable properties
-        foreach ($this->getAllowedProperties() as $prop)
-        {
-            // Get the method
-            $method = 'set' . ucfirst($prop);
+		// Get fillable properties
+		foreach ($this->getAllowedProperties() as $prop) {
+			// Get the method
+			$method = 'set' . ucfirst($prop);
 
-            // get the value
-            $value = in_array($prop, array_keys($custom)) ? $custom[$prop] : config('excel.properties.' . $prop, null);
+			// get the value
+			$value = in_array($prop, array_keys($custom)) ? $custom[$prop] : config('excel.properties.' . $prop, null);
 
-            // set the property
-            call_user_func_array([$properties, $method], [$value]);
-        }
-    }
+			// set the property
+			call_user_func_array([$properties, $method], [$value]);
+		}
+	}
 
-    /**
-     * load info from parent obj
-     * @param \PHPExcel $object
-     * @return $this
-     */
-    function cloneParent(\PHPExcel $object)
-    {
-        // Init new reflection object
-        $class = new \ReflectionClass(get_class($object));
+	/**
+	 * load info from parent obj
+	 * @param PHPOffice_PHPExcel $object
+	 * @return $this
+	 * @throws \ReflectionException
+	 */
+	function cloneParent(\PhpOffice\PhpSpreadsheet\Spreadsheet $object)
+	{
+		// Init new reflection object
+		$class = new \ReflectionClass(get_class($object));
 
-        // Loop through all properties
-        foreach($class->getProperties() as $property)
-        {
-            // Make the property public
-            $property->setAccessible(true);
+		// Loop through all properties
+		foreach ($class->getProperties() as $property) {
+			// Make the property public
+			$property->setAccessible(true);
 
-            // Set the found value to this sheet
-            $property->setValue(
-                $this,
-                $property->getValue($object)
-            );
-        }
+			// Set the found value to this sheet
+			$property->setValue(
+				$this,
+				$property->getValue($object)
+			);
+		}
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Return all allowed properties
-     * @return array
-     */
-    public function getAllowedProperties()
-    {
-        return $this->allowedProperties;
-    }
+	/**
+	 * Return all allowed properties
+	 * @return array
+	 */
+	public function getAllowedProperties()
+	{
+		return $this->allowedProperties;
+	}
 }
