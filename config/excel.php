b6c614c205c405ece2de09b23ab316322473f7b8
Patrick Brouwers <patrickbrouwersfilm@gmail.com>
2018-03-05 13:42:58 +0100

    Csv write (delimiter etc) settings configurable

--- config/excel.php
+++ config/excel.php
@@ -24,5 +24,22 @@ return [
         |
         */
         'temp_path'  => sys_get_temp_dir(),
-    ],
+
+        /*
+        |--------------------------------------------------------------------------
+        | CSV Settings
+        |--------------------------------------------------------------------------
+        |
+        | Configure e.g. delimiter, enclosure and line ending for CSV exports.
+        |
+        */
+        'csv'        => [
+            'delimiter'              => ',',
+            'enclosure'              => '"',
+            'line_ending'            => PHP_EOL,
+            'use_bom'                => false,
+            'include_separator_line' => false,
+            'excel_compatibility'    => false,
+        ],
+    ]
 ];
--- src/Writer.php
+++ src/Writer.php
@@ -9,6 +9,7 @@ use Maatwebsite\Excel\Concerns\WithEvents;
 use Maatwebsite\Excel\Events\BeforeExport;
 use Maatwebsite\Excel\Events\BeforeWriting;
 use Maatwebsite\Excel\Concerns\WithMultipleSheets;
+use PhpOffice\PhpSpreadsheet\Writer\Csv;
 
 class Writer
 {
@@ -30,11 +31,47 @@ class Writer
     protected $tmpPath;
 
     /**
+     * @var string
+     */
+    protected $delimiter;
+
+    /**
+     * @var string
+     */
+    protected $enclosure;
+
+    /**
+     * @var string
+     */
+    protected $lineEnding;
+
+    /**
+     * @var bool
+     */
+    protected $useBom;
+
+    /**
+     * @var bool
+     */
+    protected $includeSeparatorLine;
+
+    /**
+     * @var bool
+     */
+    protected $excelCompatibility;
+
+    /**
      * New Writer instance.
      */
     public function __construct()
     {
-        $this->tmpPath = config('excel.exports.temp_path', sys_get_temp_dir());
+        $this->tmpPath              = config('excel.exports.temp_path', sys_get_temp_dir());
+        $this->delimiter            = config('excel.exports.csv.delimiter', ',');
+        $this->enclosure            = config('excel.exports.csv.enclosure', '"');
+        $this->lineEnding           = config('excel.exports.csv.line_ending', PHP_EOL);
+        $this->useBom               = config('excel.exports.csv.use_bom', false);
+        $this->includeSeparatorLine = config('excel.exports.csv.include_separator_line', false);
+        $this->excelCompatibility   = config('excel.exports.csv.excel_compatibility', false);
     }
 
     /**
@@ -112,9 +149,78 @@ class Writer
         $fileName = $this->tempFile();
 
         $writer = IOFactory::createWriter($this->spreadsheet, $writerType);
+
+        if ($writer instanceof Csv) {
+            $writer->setDelimiter($this->delimiter);
+            $writer->setEnclosure($this->enclosure);
+            $writer->setLineEnding($this->lineEnding);
+            $writer->setIncludeSeparatorLine($this->includeSeparatorLine);
+            $writer->setExcelCompatibility($this->excelCompatibility);
+        }
+
         $writer->save($fileName);
 
         return $fileName;
+    }
+
+    /**
+     * @param string $delimiter
+     *
+     * @return Writer
+     */
+    public function setDelimiter(string $delimiter)
+    {
+        $this->delimiter = $delimiter;
+
+        return $this;
+    }
+
+    /**
+     * @param string $enclosure
+     *
+     * @return Writer
+     */
+    public function setEnclosure(string $enclosure)
+    {
+        $this->enclosure = $enclosure;
+
+        return $this;
+    }
+
+    /**
+     * @param string $lineEnding
+     *
+     * @return Writer
+     */
+    public function setLineEnding(string $lineEnding)
+    {
+        $this->lineEnding = $lineEnding;
+
+        return $this;
+    }
+
+    /**
+     * @param bool $includeSeparatorLine
+     *
+     * @return Writer
+     */
+    public function setIncludeSeparatorLine(bool $includeSeparatorLine)
+    {
+        $this->includeSeparatorLine = $includeSeparatorLine;
+
+        return $this;
+    }
+
+    /**
+     * @param bool $excelCompatibility
+     *
+     * @return Writer
+     */
+    public function setExcelCompatibility(bool $excelCompatibility)
+    {
+        $this->excelCompatibility = $excelCompatibility;
+
+        return $this;
     }
 
     /**
