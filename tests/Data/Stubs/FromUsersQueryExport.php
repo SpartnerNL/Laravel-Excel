25fe0033c01fab89621b3cd773e68913c8ad95cb
Patrick Brouwers <patrickbrouwersfilm@gmail.com>
2018-03-07 16:54:49 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- tests/Data/Stubs/FromUsersQueryExport.php
+++ tests/Data/Stubs/FromUsersQueryExport.php
@@ -4,11 +4,11 @@ namespace Maatwebsite\Excel\Tests\Data\Stubs;
 
 use Illuminate\Database\Query\Builder;
 use Maatwebsite\Excel\Concerns\FromQuery;
-use Maatwebsite\Excel\Concerns\Exportable;
-use Illuminate\Contracts\Support\Arrayable;
-use Maatwebsite\Excel\Concerns\WithEvents;
-use Maatwebsite\Excel\Concerns\WithMapping;
 use Maatwebsite\Excel\Events\BeforeSheet;
+use Maatwebsite\Excel\Concerns\Exportable;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Illuminate\Contracts\Support\Arrayable;
+use Maatwebsite\Excel\Concerns\WithMapping;
 use Maatwebsite\Excel\Tests\Data\Stubs\Database\User;
 
 class FromUsersQueryExport implements FromQuery, WithMapping, WithEvents
@@ -41,7 +41,7 @@ class FromUsersQueryExport implements FromQuery, WithMapping, WithEvents
         return [
             BeforeSheet::class => function (BeforeSheet $event) {
                 $event->sheet->chunkSize(10);
-            }
+            },
         ];
     }
 }
