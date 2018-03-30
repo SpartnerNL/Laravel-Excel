c3eb19ff34db0943321bef674d85db3d18a041f9
Mathias E <lk77@users.noreply.github.com>
2018-03-30 11:02:19 +0200

    Adding withHeadings to StoreCollection

--- src/Mixins/StoreCollection.php
+++ src/Mixins/StoreCollection.php
@@ -5,6 +5,7 @@ namespace Maatwebsite\Excel\Mixins;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\Exportable;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
 class StoreCollection
 {
@@ -13,9 +14,14 @@ class StoreCollection
      */
     public function storeExcel()
     {
-        return function (string $filePath, string $disk = null, string $writerType = null) {
-            $export = new class($this) implements FromCollection {
+        return function (string $filePath, string $disk = null, string $writerType = null, $withHeadings = false) {
+            $export = new class($this) implements FromCollection, WithHeadings {
                 use Exportable;
+
+                /**
+                 * @var bool
+                 */
+                public $withHeadings = false;
 
                 /**
                  * @var Collection
@@ -37,7 +43,17 @@ class StoreCollection
                 {
                     return $this->collection;
                 }
+
+                /**
+                 * @return array
+                 */
+                public function headings(): array
+                {
+                    return $this->withHeadings ? $this->collection->collapse()->keys()->all() : [];
+                }
             };
+
+            $export->withHeadings = $withHeadings;
 
             return $export->store($filePath, $disk, $writerType);
         };
