f79778c6da6f97d7e709a7ea4460550ae5220614
Mathias Eudeline <m.eudeline@crown.fr>
2018-03-30 12:25:41 +0200

    Adding tests for headings in DownloadCollection and StoreCollection

--- tests/Mixins/DownloadCollectionTest.php
+++ tests/Mixins/DownloadCollectionTest.php
@@ -3,6 +3,7 @@
 namespace Maatwebsite\Excel\Tests\Mixins;
 
 use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Excel;
 use Maatwebsite\Excel\Tests\TestCase;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
@@ -23,6 +24,28 @@ class DownloadCollectionTest extends TestCase
         $this->assertInstanceOf(BinaryFileResponse::class, $response);
         $this->assertEquals(
             'attachment; filename="collection-download.xlsx"',
+            $response->headers->get('Content-Disposition')
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function can_store_a_collection_with_headers_as_excel()
+    {
+        $collection = new Collection([
+            ['column_1' => 'test','column_2' => 'test'],
+            ['column_1' => 'test','column_2' => 'test'],
+        ]);
+
+        $response = $collection->downloadExcel('collection-headers-download.xlsx', Excel::XLSX, true);
+
+        $array = $this->readAsArray($response->getFile()->getPathName(), Excel::XLSX);
+
+        $this->assertEquals($collection->collapse()->keys()->all(), collect($array)->first());
+        $this->assertInstanceOf(BinaryFileResponse::class, $response);
+        $this->assertEquals(
+            'attachment; filename="collection-headers-download.xlsx"',
             $response->headers->get('Content-Disposition')
         );
     }
--- tests/Mixins/StoreCollectionTest.php
+++ tests/Mixins/StoreCollectionTest.php
@@ -3,6 +3,7 @@
 namespace Maatwebsite\Excel\Tests\Mixins;
 
 use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Excel;
 use Maatwebsite\Excel\Tests\TestCase;
 
 class StoreCollectionTest extends TestCase
@@ -37,5 +38,28 @@ class StoreCollectionTest extends TestCase
 
         $this->assertTrue($response);
         $this->assertFileExists(__DIR__ . '/../Data/Disks/Test/collection-store.xlsx');
+    }
+
+    /**
+     * @test
+     */
+    public function can_store_a_collection_with_headings_as_excel()
+    {
+        $collection = new Collection([
+            ['column_1' => 'test','column_2' => 'test'],
+            ['column_1' => 'test','column_2' => 'test'],
+        ]);
+
+        $response = $collection->storeExcel('collection-headers-store.xlsx', null, Excel::XLSX, true);
+
+        $file = __DIR__ . '/../Data/Disks/Local/collection-headers-store.xlsx';
+
+        $this->assertTrue($response);
+        $this->assertFileExists($file);
+        if(is_file($file))
+        {
+            $array = $this->readAsArray($file, Excel::XLSX);
+            $this->assertEquals($collection->collapse()->keys()->all(), collect($array)->first());
+        }
     }
 }
