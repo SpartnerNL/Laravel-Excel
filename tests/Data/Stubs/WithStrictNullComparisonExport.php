d320a7645283f47d418d159cc5ed13ff67b5d863
Amit Chaudhari <a-chaudhari@users.noreply.github.com>
2018-04-04 12:14:06 -0400

    additional styleci fix

--- tests/Data/Stubs/WithStrictNullComparisonExport.php
+++ tests/Data/Stubs/WithStrictNullComparisonExport.php
@@ -4,8 +4,8 @@ namespace Maatwebsite\Excel\Tests\Data\Stubs;
 
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\Exportable;
-use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithStrictNullComparison;
 
 class WithStrictNullComparisonExport implements FromCollection, WithHeadings, WithStrictNullComparison
