9667380c500f8894cda6e2a639b73c6af687fc8a
Patrick Brouwers <patrickbrouwersfilm@gmail.com>
2019-03-19 11:41:40 +0100

    Write all html sheets, fixes #1887

--- src/Factories/WriterFactory.php
+++ src/Factories/WriterFactory.php
@@ -6,6 +6,7 @@ use PhpOffice\PhpSpreadsheet\IOFactory;
 use PhpOffice\PhpSpreadsheet\Writer\Csv;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use Maatwebsite\Excel\Concerns\WithCharts;
+use PhpOffice\PhpSpreadsheet\Writer\Html;
 use PhpOffice\PhpSpreadsheet\Writer\IWriter;
 use Maatwebsite\Excel\Concerns\MapsCsvSettings;
 use Maatwebsite\Excel\Concerns\WithMultipleSheets;
@@ -30,6 +31,10 @@ class WriterFactory
 
         if (static::includesCharts($export)) {
             $writer->setIncludeCharts(true);
+        }
+
+        if ($writer instanceof Html && $export instanceof WithMultipleSheets) {
+            $writer->writeAllSheets();
         }
 
         if ($writer instanceof Csv) {
