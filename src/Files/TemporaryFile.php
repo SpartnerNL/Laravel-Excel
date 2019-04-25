08c7d358e2249fb28a5fe223b7a390ed23b27a03
Pete Frost <pete@coinvestor.co.uk>
2019-04-25 14:14:07 +0100

    Only open file in read-mode, because it's only going to be read. https://github.com/Maatwebsite/Laravel-Excel/issues/2159

--- src/Files/TemporaryFile.php
+++ src/Files/TemporaryFile.php
@@ -53,9 +53,9 @@ abstract class TemporaryFile
     public function copyFrom($filePath, string $disk = null): TemporaryFile
     {
         if ($filePath instanceof UploadedFile) {
-            $readStream = fopen($filePath->getRealPath(), 'rb+');
+            $readStream = fopen($filePath->getRealPath(), 'rb');
         } elseif ($disk === null && realpath($filePath) !== false) {
-            $readStream = fopen($filePath, 'rb+');
+            $readStream = fopen($filePath, 'rb');
         } else {
             $readStream = app('filesystem')->disk($disk)->readStream($filePath);
         }
