0bad90e6ee35c7635ecf7bbb83d1ebd2df5143a8
zacksleo <zacksleo@gmail.com>
2019-05-22 15:34:15 +0800

    Added tests for QueuedViewExport

---
+++ tests/QueuedViewExportTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Maatwebsite\Excel\Tests;
+
+use Maatwebsite\Excel\Tests\Data\Stubs\Database\User;
+use Maatwebsite\Excel\Tests\Data\Stubs\AfterQueueExportJob;
+use Maatwebsite\Excel\Tests\Data\Stubs\SheetForUsersFromView;
+
+class QueuedViewExportTest extends TestCase
+{
+    /**
+     * Setup the test environment.
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->loadLaravelMigrations(['--database' => 'testing']);
+        $this->withFactories(__DIR__ . '/Data/Stubs/Database/Factories');
+    }
+
+    /**
+     * @test
+     */
+    public function can_queue_an_export()
+    {
+        $users = factory(User::class)->times(100)->create([]);
+        $export = new SheetForUsersFromView($users);
+
+        $export->queue('queued-view-export.xlsx')->chain([
+            new AfterQueueExportJob(__DIR__ . '/Data/Disks/Local/queued-view-export.xlsx'),
+        ]);
+
+        $actual = $this->readAsArray(__DIR__ . '/Data/Disks/Local/queued-view-export.xlsx', 'Xlsx');
+
+        $this->assertCount(100, $actual);
+
+    }
+}
