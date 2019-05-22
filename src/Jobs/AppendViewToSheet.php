5d889cbe57bb4b917ad49c8dc300eb921fb763f5
zacksleo <zacksleo@gmail.com>
2019-05-22 14:40:50 +0800

    Added Queue Support for FromView Export
    fixed (Maatwebsite#1795)

---
+++ src/Jobs/AppendViewToSheet.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Maatwebsite\Excel\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Maatwebsite\Excel\Writer;
+use Maatwebsite\Excel\Files\TemporaryFile;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class AppendViewToSheet implements ShouldQueue
+{
+    use Queueable, Dispatchable;
+
+    /**
+     * @var string
+     */
+    public $temporaryFile;
+
+    /**
+     * @var string
+     */
+    public $writerType;
+
+    /**
+     * @var int
+     */
+    public $sheetIndex;
+
+    /**
+     * @var object
+     */
+    public $sheetExport;
+
+    /**
+     * @param object        $sheetExport
+     * @param TemporaryFile $temporaryFile
+     * @param string        $writerType
+     * @param int           $sheetIndex
+     * @param array         $data
+     */
+    public function __construct($sheetExport, TemporaryFile $temporaryFile, string $writerType, int $sheetIndex)
+    {
+        $this->sheetExport   = $sheetExport;
+        $this->temporaryFile = $temporaryFile;
+        $this->writerType    = $writerType;
+        $this->sheetIndex    = $sheetIndex;
+    }
+
+    /**
+     * @param Writer $writer
+     *
+     * @throws \PhpOffice\PhpSpreadsheet\Exception
+     * @throws \PhpOffice\PhpSpreadsheet\Reader\Exception
+     */
+    public function handle(Writer $writer)
+    {
+        $writer = $writer->reopen($this->temporaryFile, $this->writerType);
+
+        $sheet = $writer->getSheetByIndex($this->sheetIndex);
+
+        $sheet->fromView($this->sheetExport);
+
+        $writer->write($this->sheetExport, $this->temporaryFile, $this->writerType);
+    }
+}
