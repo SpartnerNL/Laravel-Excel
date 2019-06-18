c16e54f9ee87a758ff68d8e9e006dc2d96791db3
zacksleo <zacksleo@gmail.com>
2019-06-18 23:09:06 +0800

    tests(jobs): add test for queued FromView Export with multiple sheets

---
+++ tests/Data/Stubs/FromViewExportWithMultipleSheets.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Maatwebsite\Excel\Tests\Data\Stubs;
+
+use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\Exportable;
+use Maatwebsite\Excel\Concerns\WithMultipleSheets;
+
+class FromViewExportWithMultipleSheets implements WithMultipleSheets
+{
+    use Exportable;
+
+    /**
+     * @var Collection
+     */
+    protected $users;
+
+    /**
+     * @param Collection $users
+     */
+    public function __construct(Collection $users)
+    {
+        $this->users = $users;
+    }
+
+    /**
+     * @return SheetForUsersFromView[]
+     */
+    public function sheets() : array
+    {
+        return [
+            new SheetForUsersFromView($this->users->forPage(1, 100)),
+            new SheetForUsersFromView($this->users->forPage(2, 100)),
+            new SheetForUsersFromView($this->users->forPage(3, 100)),
+        ];
+    }
+}
