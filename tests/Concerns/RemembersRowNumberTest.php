e43e88128d538eea903e5a5cd8100fa37c5b3f93
Keno Wessels <k.wessels@demv.de>
2020-04-23 15:58:20 +0200

    StyleCi

--- tests/Concerns/RemembersRowNumberTest.php
+++ tests/Concerns/RemembersRowNumberTest.php
@@ -75,7 +75,6 @@ class RemembersRowNumberTest extends TestCase
         $this->assertEquals([46, 47, 48, 49, 50, 51, 52, 53, 54, 55], array_slice($import->rowNumbers, 45, 10));
     }
 
-
     /**
      * @test
      */
