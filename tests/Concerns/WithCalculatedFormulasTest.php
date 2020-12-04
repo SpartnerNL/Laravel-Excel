b0f2f2d36862afffb69e759bbcdb90381220ccf4
Esben Petersen <esbenspetersen@gmail.com>
2020-12-04 14:05:17 +0100

    fix style

--- tests/Concerns/WithCalculatedFormulasTest.php
+++ tests/Concerns/WithCalculatedFormulasTest.php
@@ -3,10 +3,10 @@
 namespace Maatwebsite\Excel\Tests\Concerns;
 
 use Illuminate\Database\Eloquent\Model;
+use Maatwebsite\Excel\Concerns\HasReferencesToOtherSheets;
 use Maatwebsite\Excel\Concerns\Importable;
 use Maatwebsite\Excel\Concerns\ToArray;
 use Maatwebsite\Excel\Concerns\ToModel;
-use Maatwebsite\Excel\Concerns\HasReferencesToOtherSheets;
 use Maatwebsite\Excel\Concerns\WithCalculatedFormulas;
 use Maatwebsite\Excel\Concerns\WithMultipleSheets;
 use Maatwebsite\Excel\Concerns\WithStartRow;
