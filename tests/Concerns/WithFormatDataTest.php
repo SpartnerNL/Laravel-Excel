9be1e5dc2224039214b976e2b50abdd8d3e69ac1
jason <jason_chang@y5works.com>
2021-05-14 09:36:39 +0800

    WithFormatDataTest

--- tests/Concerns/WithFormatDataTest.php
+++ tests/Concerns/WithFormatDataTest.php
@@ -2,8 +2,11 @@
 
 namespace Maatwebsite\Excel\Tests\Concerns;
 
+use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\Importable;
 use Maatwebsite\Excel\Concerns\ToArray;
+use Maatwebsite\Excel\Concerns\ToCollection;
+use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithFormatData;
 use Maatwebsite\Excel\Tests\TestCase;
 use PHPUnit\Framework\Assert;
@@ -13,7 +16,7 @@ class WithFormatDataTest extends TestCase
     /**
      * @test
      */
-    public function by_default_does_not_turn_on()
+    public function by_default_import_to_array()
     {
         $import = new class implements ToArray
         {
@@ -42,6 +45,7 @@ class WithFormatDataTest extends TestCase
      */
     public function can_import_to_array_with_format_data()
     {
+        config()->set('excel.imports.read_only', false);
         $import = new class implements ToArray, WithFormatData
         {
             use Importable;
@@ -56,6 +60,132 @@ class WithFormatDataTest extends TestCase
                 $this->called = true;
 
                 Assert::assertSame('5/12/2021', $array[0][0]);
+            }
+        };
+
+        $import->import('import-format-data.xlsx');
+
+        $this->assertTrue($import->called);
+    }
+
+    /**
+     * @test
+     */
+    public function by_default_import_to_collection()
+    {
+        $import = new class implements ToCollection
+        {
+            use Importable;
+
+            public $called = false;
+
+            /**
+             * @param array $row
+             *
+             * @return Model|null
+             */
+            public function collection(collection $collection)
+            {
+                $this->called = true;
+
+                Assert::assertSame(44328, $collection[0][0]);
+
+                return null;
+            }
+        };
+
+        $import->import('import-format-data.xlsx');
+
+        $this->assertTrue($import->called);
+    }
+
+    /**
+     * @test
+     */
+    public function can_import_to_collection_with_format_data()
+    {
+        config()->set('excel.imports.read_only', false);
+        $import = new class implements ToCollection, WithFormatData
+        {
+            use Importable;
+
+            public $called = false;
+
+            /**
+             * @param array $row
+             *
+             * @return Model|null
+             */
+            public function collection(collection $collection)
+            {
+                $this->called = true;
+
+                Assert::assertSame('5/12/2021', $collection[0][0]);
+
+                return null;
+            }
+        };
+
+        $import->import('import-format-data.xlsx');
+
+        $this->assertTrue($import->called);
+    }
+
+    /**
+     * @test
+     */
+    public function by_default_import_to_model()
+    {
+        $import = new class implements ToModel
+        {
+            use Importable;
+
+            public $called = false;
+
+            /**
+             * @param array $row
+             *
+             * @return Model|null
+             */
+            public function model(array $row)
+            {
+                $this->called = true;
+
+                Assert::assertSame(44328, $row[0]);
+
+                return null;
+            }
+        };
+
+        $import->import('import-format-data.xlsx');
+
+        $this->assertTrue($import->called);
+    }
+
+    /**
+     * @test
+     */
+    public function can_import_to_model_with_format_data()
+    {
+        config()->set('excel.imports.read_only', false);
+        $import = new class implements ToModel, WithFormatData
+        {
+            use Importable;
+
+            public $called = false;
+
+            /**
+             * @param array $row
+             *
+             * @return Model|null
+             */
+            public function model(array $row)
+            {
+                $this->called = true;
+
+                Assert::assertSame('5/12/2021', $row[0]);
+
+                return null;
             }
         };
 
