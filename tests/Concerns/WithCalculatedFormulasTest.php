df280f89aef8d5cc37d2ae0baa7535b3d8cc5f49
Rodolfo Sacchi <rodolfo@agrihp.com>
2021-05-31 08:16:09 +0200

    Add test units for WithCalculatedFormulas and SkipsEmptyRows together

--- tests/Concerns/WithCalculatedFormulasTest.php
+++ tests/Concerns/WithCalculatedFormulasTest.php
@@ -10,6 +10,7 @@ use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithCalculatedFormulas;
 use Maatwebsite\Excel\Concerns\WithMultipleSheets;
 use Maatwebsite\Excel\Concerns\WithStartRow;
+use Maatwebsite\Excel\Concerns\SkipsEmptyRows;
 use Maatwebsite\Excel\Tests\TestCase;
 use PHPUnit\Framework\Assert;
 
@@ -174,5 +175,63 @@ class WithCalculatedFormulasTest extends TestCase
         };
 
         $import->import('import-formulas-multiple-sheets.xlsx');
+    }
+
+    /**
+     * @test
+     */
+    public function can_import_to_array_with_calculated_formulas_and_skips_empty()
+    {
+        $import = new class implements ToArray, WithCalculatedFormulas, SkipsEmptyRows
+        {
+            use Importable;
+
+            public $called = false;
+
+            /**
+             * @param array $array
+             */
+            public function array(array $array)
+            {
+                $this->called = true;
+
+                Assert::assertSame(2, $array[0][0]);
+            }
+        };
+
+        $import->import('import-formulas.xlsx');
+
+        $this->assertTrue($import->called);
+    }
+
+    /**
+     * @test
+     */
+    public function can_import_to_model_with_calculated_formulas_and_skips_empty()
+    {
+        $import = new class implements ToModel, WithCalculatedFormulas, SkipsEmptyRows
+        {
+            use Importable;
+
+            public $called = false;
+
+            /**
+             * @param array $row
+             *
+             * @return Model|null
+             */
+            public function model(array $row)
+            {
+                $this->called = true;
+
+                Assert::assertSame(2, $row[0]);
+
+                return null;
+            }
+        };
+
+        $import->import('import-formulas.xlsx');
+
+        $this->assertTrue($import->called);
     }
 }
