0ee01a95d5caa1694c10d220446acbf349e374b5
Sven Rymenants <sven@byteworks.be>
2022-10-20 18:54:53 +0200

    Fix code styling

--- tests/Helpers/FileHelper.php
+++ tests/Helpers/FileHelper.php
@@ -9,14 +9,15 @@ class FileHelper
         return config('filesystems.disks.' . $diskName . '.root') . DIRECTORY_SEPARATOR . $fileName;
     }
 
-    public static function recursiveDelete($fileName) {
+    public static function recursiveDelete($fileName)
+    {
         if (is_file($fileName)) {
             return @unlink($fileName);
         }
-        
+
         if (is_dir($fileName)) {
-            $scan = glob(rtrim($fileName,'/').'/*');
-            foreach ($scan as $path){
+            $scan = glob(rtrim($fileName, '/') . '/*');
+            foreach ($scan as $path) {
                 self::recursiveDelete($path);
             }
             return @rmdir($fileName);
