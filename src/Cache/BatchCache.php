7dacd7d587f3671fa0b234f220daa81d2a52cdc8
Patrick Brouwers <patrickbrouwersfilm@gmail.com>
2023-01-02 17:07:53 +0100

    Attempt to fix the psr mess for now

--- src/Cache/BatchCache.php
+++ src/Cache/BatchCache.php
@@ -2,141 +2,281 @@
 
 namespace Maatwebsite\Excel\Cache;
 
+use Composer\Semver\VersionParser;
 use Psr\SimpleCache\CacheInterface;
 
-class BatchCache implements CacheInterface
-{
-    /**
-     * @var CacheInterface
-     */
-    protected $cache;
-
-    /**
-     * @var MemoryCache
-     */
-    protected $memory;
-
-    /**
-     * @param  CacheInterface  $cache
-     * @param  MemoryCache  $memory
-     */
-    public function __construct(CacheInterface $cache, MemoryCache $memory)
+if (\Composer\InstalledVersions::satisfies(new VersionParser, 'psr/cache', '^3.0')) {
+    class BatchCache implements CacheInterface
     {
-        $this->cache  = $cache;
-        $this->memory = $memory;
-    }
+        /**
+         * @var CacheInterface
+         */
+        protected $cache;
 
-    /**
-     * {@inheritdoc}
-     */
-    public function get($key, $default = null)
-    {
-        if ($this->memory->has($key)) {
-            return $this->memory->get($key);
+        /**
+         * @var MemoryCache
+         */
+        protected $memory;
+
+        /**
+         * @param CacheInterface $cache
+         * @param MemoryCache    $memory
+         */
+        public function __construct(CacheInterface $cache, MemoryCache $memory)
+        {
+            $this->cache  = $cache;
+            $this->memory = $memory;
         }
 
-        return $this->cache->get($key, $default);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function set($key, $value, $ttl = null)
-    {
-        $this->memory->set($key, $value, $ttl);
-
-        if ($this->memory->reachedMemoryLimit()) {
-            return $this->cache->setMultiple($this->memory->flush(), $ttl);
-        }
-
-        return true;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function delete($key)
-    {
-        if ($this->memory->has($key)) {
-            return $this->memory->delete($key);
-        }
-
-        return $this->cache->delete($key);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function clear()
-    {
-        $this->memory->clear();
-
-        return $this->cache->clear();
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getMultiple($keys, $default = null)
-    {
-        // Check if all keys are still in memory
-        $memory              = $this->memory->getMultiple($keys, $default);
-        $actualItemsInMemory = count(array_filter($memory));
-
-        if ($actualItemsInMemory === count($keys)) {
-            return $memory;
-        }
-
-        // Get all rows from cache if none is hold in memory.
-        if ($actualItemsInMemory === 0) {
-            return $this->cache->getMultiple($keys, $default);
-        }
-
-        // Add missing values from cache.
-        foreach ($this->cache->getMultiple($keys, $default) as $key => $value) {
-            if (null !== $value) {
-                $memory[$key] = $value;
+        /**
+         * {@inheritdoc}
+         */
+        public function get(string $key, mixed $default = null): mixed
+        {
+            if ($this->memory->has($key)) {
+                return $this->memory->get($key);
             }
+
+            return $this->cache->get($key, $default);
         }
 
-        return $memory;
-    }
+        /**
+         * {@inheritdoc}
+         */
+        public function set(string $key, mixed $value, null|int|\DateInterval $ttl = null): bool
+        {
+            $this->memory->set($key, $value, $ttl);
 
-    /**
-     * {@inheritdoc}
-     */
-    public function setMultiple($values, $ttl = null)
-    {
-        $this->memory->setMultiple($values, $ttl);
+            if ($this->memory->reachedMemoryLimit()) {
+                return $this->cache->setMultiple($this->memory->flush(), $ttl);
+            }
 
-        if ($this->memory->reachedMemoryLimit()) {
-            return $this->cache->setMultiple($this->memory->flush(), $ttl);
-        }
-
-        return true;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function deleteMultiple($keys)
-    {
-        $keys = is_array($keys) ? $keys : iterator_to_array($keys);
-
-        $this->memory->deleteMultiple($keys);
-
-        return $this->cache->deleteMultiple($keys);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function has($key)
-    {
-        if ($this->memory->has($key)) {
             return true;
         }
 
-        return $this->cache->has($key);
+        /**
+         * {@inheritdoc}
+         */
+        public function delete(string $key): bool
+        {
+            if ($this->memory->has($key)) {
+                return $this->memory->delete($key);
+            }
+
+            return $this->cache->delete($key);
+        }
+
+        /**
+         * {@inheritdoc}
+         */
+        public function clear(): bool
+        {
+            $this->memory->clear();
+
+            return $this->cache->clear();
+        }
+
+        /**
+         * {@inheritdoc}
+         */
+        public function getMultiple(iterable $keys, mixed $default = null): iterable
+        {
+            // Check if all keys are still in memory
+            $memory              = $this->memory->getMultiple($keys, $default);
+            $actualItemsInMemory = count(array_filter($memory));
+
+            if ($actualItemsInMemory === count($keys)) {
+                return $memory;
+            }
+
+            // Get all rows from cache if none is hold in memory.
+            if ($actualItemsInMemory === 0) {
+                return $this->cache->getMultiple($keys, $default);
+            }
+
+            // Add missing values from cache.
+            foreach ($this->cache->getMultiple($keys, $default) as $key => $value) {
+                if (null !== $value) {
+                    $memory[$key] = $value;
+                }
+            }
+
+            return $memory;
+        }
+
+        /**
+         * {@inheritdoc}
+         */
+        public function setMultiple(iterable $values, null|int|\DateInterval $ttl = null): bool
+        {
+            $this->memory->setMultiple($values, $ttl);
+
+            if ($this->memory->reachedMemoryLimit()) {
+                return $this->cache->setMultiple($this->memory->flush(), $ttl);
+            }
+
+            return true;
+        }
+
+        /**
+         * {@inheritdoc}
+         */
+        public function deleteMultiple(iterable $keys): bool
+        {
+            $keys = is_array($keys) ? $keys : iterator_to_array($keys);
+
+            $this->memory->deleteMultiple($keys);
+
+            return $this->cache->deleteMultiple($keys);
+        }
+
+        /**
+         * {@inheritdoc}
+         */
+        public function has(string $key): bool
+        {
+            if ($this->memory->has($key)) {
+                return true;
+            }
+
+            return $this->cache->has($key);
+        }
+    }
+} else {
+    class BatchCache implements CacheInterface
+    {
+        /**
+         * @var CacheInterface
+         */
+        protected $cache;
+
+        /**
+         * @var MemoryCache
+         */
+        protected $memory;
+
+        /**
+         * @param  CacheInterface  $cache
+         * @param  MemoryCache  $memory
+         */
+        public function __construct(CacheInterface $cache, MemoryCache $memory)
+        {
+            $this->cache  = $cache;
+            $this->memory = $memory;
+        }
+
+        /**
+         * {@inheritdoc}
+         */
+        public function get($key, $default = null)
+        {
+            if ($this->memory->has($key)) {
+                return $this->memory->get($key);
+            }
+
+            return $this->cache->get($key, $default);
+        }
+
+        /**
+         * {@inheritdoc}
+         */
+        public function set($key, $value, $ttl = null)
+        {
+            $this->memory->set($key, $value, $ttl);
+
+            if ($this->memory->reachedMemoryLimit()) {
+                return $this->cache->setMultiple($this->memory->flush(), $ttl);
+            }
+
+            return true;
+        }
+
+        /**
+         * {@inheritdoc}
+         */
+        public function delete($key)
+        {
+            if ($this->memory->has($key)) {
+                return $this->memory->delete($key);
+            }
+
+            return $this->cache->delete($key);
+        }
+
+        /**
+         * {@inheritdoc}
+         */
+        public function clear()
+        {
+            $this->memory->clear();
+
+            return $this->cache->clear();
+        }
+
+        /**
+         * {@inheritdoc}
+         */
+        public function getMultiple($keys, $default = null)
+        {
+            // Check if all keys are still in memory
+            $memory              = $this->memory->getMultiple($keys, $default);
+            $actualItemsInMemory = count(array_filter($memory));
+
+            if ($actualItemsInMemory === count($keys)) {
+                return $memory;
+            }
+
+            // Get all rows from cache if none is hold in memory.
+            if ($actualItemsInMemory === 0) {
+                return $this->cache->getMultiple($keys, $default);
+            }
+
+            // Add missing values from cache.
+            foreach ($this->cache->getMultiple($keys, $default) as $key => $value) {
+                if (null !== $value) {
+                    $memory[$key] = $value;
+                }
+            }
+
+            return $memory;
+        }
+
+        /**
+         * {@inheritdoc}
+         */
+        public function setMultiple($values, $ttl = null)
+        {
+            $this->memory->setMultiple($values, $ttl);
+
+            if ($this->memory->reachedMemoryLimit()) {
+                return $this->cache->setMultiple($this->memory->flush(), $ttl);
+            }
+
+            return true;
+        }
+
+        /**
+         * {@inheritdoc}
+         */
+        public function deleteMultiple($keys)
+        {
+            $keys = is_array($keys) ? $keys : iterator_to_array($keys);
+
+            $this->memory->deleteMultiple($keys);
+
+            return $this->cache->deleteMultiple($keys);
+        }
+
+        /**
+         * {@inheritdoc}
+         */
+        public function has($key)
+        {
+            if ($this->memory->has($key)) {
+                return true;
+            }
+
+            return $this->cache->has($key);
+        }
     }
 }
--- src/Cache/MemoryCache.php
+++ src/Cache/MemoryCache.php
@@ -2,137 +2,273 @@
 
 namespace Maatwebsite\Excel\Cache;
 
+use Composer\Semver\VersionParser;
 use Psr\SimpleCache\CacheInterface;
 
-class MemoryCache implements CacheInterface
-{
-    /**
-     * @var int|null
-     */
-    protected $memoryLimit;
-
-    /**
-     * @var array
-     */
-    protected $cache = [];
-
-    /**
-     * @param  int|null  $memoryLimit
-     */
-    public function __construct(int $memoryLimit = null)
+if (\Composer\InstalledVersions::satisfies(new VersionParser, 'psr/cache', '^3.0')) {
+    class MemoryCache implements CacheInterface
     {
-        $this->memoryLimit = $memoryLimit;
-    }
+        /**
+         * @var int|null
+         */
+        protected $memoryLimit;
 
-    /**
-     * {@inheritdoc}
-     */
-    public function clear()
-    {
-        $this->cache = [];
+        /**
+         * @var array
+         */
+        protected $cache = [];
 
-        return true;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function delete($key)
-    {
-        unset($this->cache[$key]);
-
-        return true;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function deleteMultiple($keys)
-    {
-        foreach ($keys as $key) {
-            $this->delete($key);
+        /**
+         * @param int|null $memoryLimit
+         */
+        public function __construct(int $memoryLimit = null)
+        {
+            $this->memoryLimit = $memoryLimit;
         }
 
-        return true;
-    }
+        /**
+         * {@inheritdoc}
+         */
+        public function clear(): bool
+        {
+            $this->cache = [];
 
-    /**
-     * {@inheritdoc}
-     */
-    public function get($key, $default = null)
-    {
-        if ($this->has($key)) {
-            return $this->cache[$key];
+            return true;
         }
 
-        return $default;
-    }
+        /**
+         * {@inheritdoc}
+         */
+        public function delete(string $key): bool
+        {
+            unset($this->cache[$key]);
 
-    /**
-     * {@inheritdoc}
-     */
-    public function getMultiple($keys, $default = null)
-    {
-        $results = [];
-        foreach ($keys as $key) {
-            $results[$key] = $this->get($key, $default);
+            return true;
         }
 
-        return $results;
-    }
+        /**
+         * {@inheritdoc}
+         */
+        public function deleteMultiple($keys): bool
+        {
+            foreach ($keys as $key) {
+                $this->delete($key);
+            }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function has($key)
-    {
-        return isset($this->cache[$key]);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function set($key, $value, $ttl = null)
-    {
-        $this->cache[$key] = $value;
-
-        return true;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function setMultiple($values, $ttl = null)
-    {
-        foreach ($values as $key => $value) {
-            $this->set($key, $value);
+            return true;
         }
 
-        return true;
-    }
+        /**
+         * {@inheritdoc}
+         */
+        public function get(string $key, mixed $default = null): mixed
+        {
+            if ($this->has($key)) {
+                return $this->cache[$key];
+            }
 
-    /**
-     * @return bool
-     */
-    public function reachedMemoryLimit(): bool
-    {
-        // When no limit is given, we'll never reach any limit.
-        if (null === $this->memoryLimit) {
-            return false;
+            return $default;
         }
 
-        return count($this->cache) >= $this->memoryLimit;
+        /**
+         * {@inheritdoc}
+         */
+        public function getMultiple(iterable $keys, mixed $default = null): iterable
+        {
+            $results = [];
+            foreach ($keys as $key) {
+                $results[$key] = $this->get($key, $default);
+            }
+
+            return $results;
+        }
+
+        /**
+         * {@inheritdoc}
+         */
+        public function has($key): bool
+        {
+            return isset($this->cache[$key]);
+        }
+
+        /**
+         * {@inheritdoc}
+         */
+        public function set(string $key, mixed $value, null|int|\DateInterval $ttl = null): bool
+        {
+            $this->cache[$key] = $value;
+
+            return true;
+        }
+
+        /**
+         * {@inheritdoc}
+         */
+        public function setMultiple($values, $ttl = null): bool
+        {
+            foreach ($values as $key => $value) {
+                $this->set($key, $value);
+            }
+
+            return true;
+        }
+
+        /**
+         * @return bool
+         */
+        public function reachedMemoryLimit(): bool
+        {
+            // When no limit is given, we'll never reach any limit.
+            if (null === $this->memoryLimit) {
+                return false;
+            }
+
+            return count($this->cache) >= $this->memoryLimit;
+        }
+
+        /**
+         * @return array
+         */
+        public function flush(): array
+        {
+            $memory = $this->cache;
+
+            $this->clear();
+
+            return $memory;
+        }
     }
-
-    /**
-     * @return array
-     */
-    public function flush(): array
+} else {
+    class MemoryCache implements CacheInterface
     {
-        $memory = $this->cache;
+        /**
+         * @var int|null
+         */
+        protected $memoryLimit;
 
-        $this->clear();
+        /**
+         * @var array
+         */
+        protected $cache = [];
 
-        return $memory;
+        /**
+         * @param int|null $memoryLimit
+         */
+        public function __construct(int $memoryLimit = null)
+        {
+            $this->memoryLimit = $memoryLimit;
+        }
+
+        /**
+         * {@inheritdoc}
+         */
+        public function clear()
+        {
+            $this->cache = [];
+
+            return true;
+        }
+
+        /**
+         * {@inheritdoc}
+         */
+        public function delete($key)
+        {
+            unset($this->cache[$key]);
+
+            return true;
+        }
+
+        /**
+         * {@inheritdoc}
+         */
+        public function deleteMultiple($keys)
+        {
+            foreach ($keys as $key) {
+                $this->delete($key);
+            }
+
+            return true;
+        }
+
+        /**
+         * {@inheritdoc}
+         */
+        public function get($key, $default = null)
+        {
+            if ($this->has($key)) {
+                return $this->cache[$key];
+            }
+
+            return $default;
+        }
+
+        /**
+         * {@inheritdoc}
+         */
+        public function getMultiple($keys, $default = null)
+        {
+            $results = [];
+            foreach ($keys as $key) {
+                $results[$key] = $this->get($key, $default);
+            }
+
+            return $results;
+        }
+
+        /**
+         * {@inheritdoc}
+         */
+        public function has($key)
+        {
+            return isset($this->cache[$key]);
+        }
+
+        /**
+         * {@inheritdoc}
+         */
+        public function set($key, $value, $ttl = null)
+        {
+            $this->cache[$key] = $value;
+
+            return true;
+        }
+
+        /**
+         * {@inheritdoc}
+         */
+        public function setMultiple($values, $ttl = null)
+        {
+            foreach ($values as $key => $value) {
+                $this->set($key, $value);
+            }
+
+            return true;
+        }
+
+        /**
+         * @return bool
+         */
+        public function reachedMemoryLimit(): bool
+        {
+            // When no limit is given, we'll never reach any limit.
+            if (null === $this->memoryLimit) {
+                return false;
+            }
+
+            return count($this->cache) >= $this->memoryLimit;
+        }
+
+        /**
+         * @return array
+         */
+        public function flush(): array
+        {
+            $memory = $this->cache;
+
+            $this->clear();
+
+            return $memory;
+        }
     }
 }
