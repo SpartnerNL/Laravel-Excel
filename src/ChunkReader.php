92fd120b329d1e200ca989396888decc42559cc3
Damilare Anjorin <damilareanjorin1@gmail.com>
2023-09-29 12:04:34 +0100

    chore: add queue and connection property to ReadChunk from import

--- src/ChunkReader.php
+++ src/ChunkReader.php
@@ -50,7 +50,7 @@ class ChunkReader
         $totalRows    = $reader->getTotalRows();
         $worksheets   = $reader->getWorksheets($import);
         $queue        = property_exists($import, 'queue') ? $import->queue : null;
-        $delayCleanup = property_exists($import, 'delayCleanup') ? $import->delayCleanup : 600;
+        $delayCleanup = property_exists($import, 'cleanupInterval') ? $import->cleanupInterval : 60;
 
         if ($import instanceof WithProgressBar) {
             $import->getConsoleOutput()->progressStart(array_sum($totalRows));
@@ -84,6 +84,9 @@ class ChunkReader
         $afterImportJob = new AfterImportJob($import, $reader);
 
         if ($import instanceof ShouldQueueWithoutChain) {
+            $afterImportJob->setInterval($delayCleanup);
+            $afterImportJob->setDependencies($jobs);
+
             $jobs->push($afterImportJob->delay($delayCleanup));
 
             return $jobs->each(function ($job) use ($queue) {
