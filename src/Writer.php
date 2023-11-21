6283025398fa0e26f692af8a674a57ff8dcb4ae3
adam goucher <adam@goucher.ca>
2023-11-21 12:44:23 -0500

    check if local path is valid before writing
    fixes https://github.com/SpartnerNL/Laravel-Excel/issues/4033

--- src/Writer.php
+++ src/Writer.php
@@ -165,6 +165,11 @@ class Writer
             $export
         );
 
+        if ($temporaryFile instanceof RemoteTemporaryFile && !$temporaryFile->existsLocally()) {
+            $temporaryFile = resolve(TemporaryFileFactory::class)
+                ->makeLocal(Arr::last(explode('/', $temporaryFile->getLocalPath())));
+        }
+        
         $writer->save(
             $temporaryFile->getLocalPath()
         );
