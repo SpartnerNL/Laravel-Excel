2d395b3827cc0265a6b41399401706dd0accce35
KSneijders <kerwin@wildsea.nl>
2025-06-05 09:55:11 +0200

    Fixes incorrectly ordered imports

--- tests/Concerns/ToModelTest.php
+++ tests/Concerns/ToModelTest.php
@@ -10,8 +10,8 @@ use Maatwebsite\Excel\Concerns\Importable;
 use Maatwebsite\Excel\Concerns\PersistRelations;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Tests\Data\Stubs\Database\Group;
-use Maatwebsite\Excel\Tests\Data\Stubs\Database\User;
 use Maatwebsite\Excel\Tests\Data\Stubs\Database\GroupedUser;
+use Maatwebsite\Excel\Tests\Data\Stubs\Database\User;
 use Maatwebsite\Excel\Tests\TestCase;
 
 class ToModelTest extends TestCase
