752b2cb0ba244d4261062373bd6c0619760d6652
Ion Bazan <ion.bazan@gmail.com>
2026-08-19 22:08:02 +0800

    Add tests for WithCharts, WithDrawings and download buffer handling (#4427)

---
+++ tests/Concerns/WithChartsTest.php
@@ -0,0 +1,131 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Maatwebsite\Excel\Tests\Concerns;
+
+use Maatwebsite\Excel\Concerns\Exportable;
+use Maatwebsite\Excel\Concerns\FromArray;
+use Maatwebsite\Excel\Concerns\WithCharts;
+use Maatwebsite\Excel\Concerns\WithTitle;
+use Maatwebsite\Excel\Tests\TestCase;
+use PhpOffice\PhpSpreadsheet\Chart\Chart;
+use PhpOffice\PhpSpreadsheet\Chart\DataSeries;
+use PhpOffice\PhpSpreadsheet\Chart\DataSeriesValues;
+use PhpOffice\PhpSpreadsheet\Chart\Legend;
+use PhpOffice\PhpSpreadsheet\Chart\PlotArea;
+use PhpOffice\PhpSpreadsheet\Chart\Title;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+
+final class WithChartsTest extends TestCase
+{
+    public function test_can_export_with_a_single_chart(): void
+    {
+        $export = new class implements FromArray, WithCharts, WithTitle
+        {
+            use Exportable;
+
+            public function array(): array
+            {
+                return [[1], [2], [3]];
+            }
+
+            public function title(): string
+            {
+                return 'Sheet1';
+            }
+
+            public function charts(): Chart
+            {
+                return $this->buildChart('chart1');
+            }
+
+            private function buildChart(string $name): Chart
+            {
+                $series = new DataSeries(
+                    DataSeries::TYPE_BARCHART,
+                    DataSeries::GROUPING_CLUSTERED,
+                    [0],
+                    plotValues: [new DataSeriesValues(DataSeriesValues::DATASERIES_TYPE_NUMBER, 'Sheet1!$A$1:$A$3', null, 3)]
+                );
+
+                $chart = new Chart($name, new Title('Chart title'), new Legend, new PlotArea(null, [$series]));
+                $chart->setTopLeftPosition('C1');
+                $chart->setBottomRightPosition('J15');
+
+                return $chart;
+            }
+        };
+
+        $export->store('with-charts.xlsx');
+
+        $sheet = $this->readWithCharts(__DIR__ . '/../Data/Disks/Local/with-charts.xlsx');
+
+        $this->assertCount(1, $sheet->getChartCollection());
+        $this->assertSame('chart1', $sheet->getChartByIndex('0')->getName());
+    }
+
+    public function test_can_export_with_multiple_charts(): void
+    {
+        $export = new class implements FromArray, WithCharts, WithTitle
+        {
+            use Exportable;
+
+            public function array(): array
+            {
+                return [[1], [2], [3]];
+            }
+
+            public function title(): string
+            {
+                return 'Sheet1';
+            }
+
+            /**
+             * @return Chart[]
+             */
+            public function charts(): array
+            {
+                return [
+                    $this->buildChart('chart1'),
+                    $this->buildChart('chart2'),
+                ];
+            }
+
+            private function buildChart(string $name): Chart
+            {
+                $series = new DataSeries(
+                    DataSeries::TYPE_BARCHART,
+                    DataSeries::GROUPING_CLUSTERED,
+                    [0],
+                    plotValues: [new DataSeriesValues(DataSeriesValues::DATASERIES_TYPE_NUMBER, 'Sheet1!$A$1:$A$3', null, 3)]
+                );
+
+                $chart = new Chart($name, new Title('Chart title'), new Legend, new PlotArea(null, [$series]));
+                $chart->setTopLeftPosition('C1');
+                $chart->setBottomRightPosition('J15');
+
+                return $chart;
+            }
+        };
+
+        $export->store('with-charts-multiple.xlsx');
+
+        $sheet = $this->readWithCharts(__DIR__ . '/../Data/Disks/Local/with-charts-multiple.xlsx');
+
+        $this->assertCount(2, $sheet->getChartCollection());
+        $this->assertSame('chart1', $sheet->getChartByIndex('0')->getName());
+        $this->assertSame('chart2', $sheet->getChartByIndex('1')->getName());
+    }
+
+    private function readWithCharts(string $filePath): Worksheet
+    {
+        $reader = IOFactory::createReader('Xlsx');
+        $reader->setIncludeCharts(true);
+
+        $spreadsheet = $reader->load($filePath);
+
+        return $spreadsheet->getActiveSheet();
+    }
+}
---
+++ tests/Concerns/WithDrawingsTest.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Maatwebsite\Excel\Tests\Concerns;
+
+use Maatwebsite\Excel\Concerns\Exportable;
+use Maatwebsite\Excel\Concerns\FromArray;
+use Maatwebsite\Excel\Concerns\WithDrawings;
+use Maatwebsite\Excel\Tests\TestCase;
+use PhpOffice\PhpSpreadsheet\Worksheet\Drawing;
+
+final class WithDrawingsTest extends TestCase
+{
+    public function test_can_export_with_a_single_drawing(): void
+    {
+        $export = new class implements FromArray, WithDrawings
+        {
+            use Exportable;
+
+            public function array(): array
+            {
+                return [['Patrick', 'Brouwers']];
+            }
+
+            public function drawings(): Drawing
+            {
+                $drawing = new Drawing;
+                $drawing->setName('Logo');
+                $drawing->setPath(__DIR__ . '/../Data/Disks/Local/icon.jpg');
+                $drawing->setCoordinates('B2');
+
+                return $drawing;
+            }
+        };
+
+        $export->store('with-drawings.xlsx');
+
+        $spreadsheet = $this->read(__DIR__ . '/../Data/Disks/Local/with-drawings.xlsx', 'Xlsx');
+        $sheet       = $spreadsheet->getActiveSheet();
+
+        $this->assertCount(1, $sheet->getDrawingCollection());
+        $this->assertSame('Logo', $sheet->getDrawingCollection()[0]->getName());
+        $this->assertSame('B2', $sheet->getDrawingCollection()[0]->getCoordinates());
+    }
+
+    public function test_can_export_with_multiple_drawings(): void
+    {
+        $export = new class implements FromArray, WithDrawings
+        {
+            use Exportable;
+
+            public function array(): array
+            {
+                return [['Patrick', 'Brouwers']];
+            }
+
+            /**
+             * @return Drawing[]
+             */
+            public function drawings(): array
+            {
+                $first = new Drawing;
+                $first->setName('First');
+                $first->setPath(__DIR__ . '/../Data/Disks/Local/icon.jpg');
+                $first->setCoordinates('B2');
+
+                $second = new Drawing;
+                $second->setName('Second');
+                $second->setPath(__DIR__ . '/../Data/Disks/Local/avatar.jpg');
+                $second->setCoordinates('D2');
+
+                return [$first, $second];
+            }
+        };
+
+        $export->store('with-drawings-multiple.xlsx');
+
+        $spreadsheet = $this->read(__DIR__ . '/../Data/Disks/Local/with-drawings-multiple.xlsx', 'Xlsx');
+        $sheet       = $spreadsheet->getActiveSheet();
+
+        $this->assertCount(2, $sheet->getDrawingCollection());
+        $this->assertSame('First', $sheet->getDrawingCollection()[0]->getName());
+        $this->assertSame('Second', $sheet->getDrawingCollection()[1]->getName());
+    }
+}
